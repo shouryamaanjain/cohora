d092ff16ededffbf7dd76790f77810e7f847f8ad
shouryamaanjain <jainshouryamaan@gmail.com>
2025-12-26 14:49:21 +0530

    Fix TypeScript errors for Vercel deployment

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,97 +1,30 @@
-import * as React from "react";
-import { Tooltip as TooltipPrimitive } from "@base-ui-components/react/tooltip";
+import * as React from "react"
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-function TooltipProvider({
-  delay = 0,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
-  return (
-    <TooltipPrimitive.Provider
-      data-slot="tooltip-provider"
-      delay={delay}
-      {...props}
-    />
-  );
-}
+const TooltipProvider = TooltipPrimitive.Provider
 
-function Tooltip({
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
-  return (
-    <TooltipProvider>
-      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
-    </TooltipProvider>
-  );
-}
+const Tooltip = TooltipPrimitive.Root
 
-function TooltipTrigger({
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />;
-}
+const TooltipTrigger = TooltipPrimitive.Trigger
 
-function TooltipPositioner({
-  className,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Positioner>) {
-  return (
-    <TooltipPrimitive.Portal>
-      <TooltipPrimitive.Positioner
-        data-slot="tooltip-positioner"
-        sideOffset={8}
-        className={cn("z-50", className)}
-        {...props}
-      />
-    </TooltipPrimitive.Portal>
-  );
-}
-
-function TooltipContent({
-  className,
-  children,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Popup>) {
-  return (
-    <TooltipPrimitive.Popup
-      data-slot="tooltip-content"
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
       className={cn(
-        "bg-primary text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[closed]:animate-out data-[closed]:fade-out-0 data-[closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-fit origin-(--radix-tooltip-content-transform-origin) rounded-md px-3 py-1.5 text-xs text-balance",
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <TooltipArrow />
-    </TooltipPrimitive.Popup>
-  );
-}
-
-function TooltipArrow({
-  className,
-  ...props
-}: React.ComponentProps<typeof TooltipPrimitive.Arrow>) {
-  return (
-    <TooltipPrimitive.Arrow
-      data-slot="tooltip-arrow"
-      className={cn(
-        "bg-primary fill-primary z-50 size-2.5 rotate-45 rounded-[2px]",
-        "data-[side=bottom]:-translate-y-1/2 data-[side=bottom]:top-px",
-        "data-[side=top]:translate-y-1/2 data-[side=top]:bottom-px",
-        "data-[side=left]:translate-x-1/2 data-[side=left]:right-px",
-        "data-[side=right]:-translate-x-1/2 data-[side=right]:left-px",
+        "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         className
       )}
       {...props}
     />
-  );
-}
+  </TooltipPrimitive.Portal>
+))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
 
-export {
-  Tooltip,
-  TooltipTrigger,
-  TooltipContent,
-  TooltipProvider,
-  TooltipPositioner,
-};
+export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider }
